e6e54e28e803711e8209d2b6c0ba1d972064da70
Anor Neto <anornetoo@gmail.com>
2021-07-25 23:17:42 -0300

    * Finish Vessel Tests

--- api/schemas/vessel_schema.py
+++ api/schemas/vessel_schema.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from .equipment_schema import EquipmentRead
 
@@ -8,6 +8,11 @@ class VesselBase(BaseModel):
   code: str
 
 class VesselCreate(VesselBase):
+  @validator("code", pre=True, always=True)
+  def check_code(cls,code_string):
+    assert bool(code_string and not code_string.isspace()), "Vessel Code cannot be empty."
+    return code_string.strip()
+
   pass;
 
 class VesselRead(VesselBase):
--- tests/test_vessels_route.py
+++ tests/test_vessels_route.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import List
+import json
 
 from fastapi.testclient import TestClient
 from fastapi import status
@@ -60,21 +61,36 @@ def post_vessel(vessel: VesselCreate):
 
 # Test Cases
 
-def test_post_created_vessel(vessel_code_fixed: VesselCreate):
+def test_post_created_vessel(vessel_code_fixed: VesselCreate): # creates vessel_code_fixed
     response = post_vessel(vessel_code_fixed)
-    assert response.status_code == 201
+    assert response.status_code == status.HTTP_201_CREATED
     json_response = response.json()
     assert json_response.get("code") == vessel_code_fixed.get("code")
     assert json_response.get("is_active") == True
 
-def test_post_conflict_vessel(vessel_code_fixed: VesselCreate):
+def test_post_conflict_vessel(vessel_code_fixed: VesselCreate): # validates conflict when trying to create vessel_code_fixed again
   response = post_vessel(vessel_code_fixed)
-  assert response.status_code == 409
+  assert response.status_code == status.HTTP_409_CONFLICT
   json_response = response.json()
   assert json_response.get("detail") == "Vessel already registered"
 
-def test_get_all_vessels():
+def test_post_empty_vessel_code(): # validates error when trying to inser empty vessel code
+  response = post_vessel({"code" : "                     "})
+  assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+  json_response = response.json()
+  assert json_response["detail"][0]["msg"] == "Vessel Code cannot be empty."
+
+
+
+def test_get_all_vessels(vessel_code_fixed: VesselCreate): # Get all vessels and check if vessel_code_fixed exists
     response = client.get(vessel_route)
     assert response.status_code == status.HTTP_200_OK
     json_response = response.json()
-    assert list(json_response) is List
+    assert any(vessel.get("code") == vessel_code_fixed.get("code") for vessel in json_response )
+
+def test_get_vessel_by_code(vessel_code_fixed: VesselCreate): # get vessel_code_fixed Vessel
+    vessel_test_code = vessel_code_fixed.get("code")
+    response = client.get(vessel_route + f"/{vessel_test_code}")
+    assert response.status_code == status.HTTP_200_OK
+    json_response = response.json()
+    assert json_response.get("code") == vessel_code_fixed.get("code")
