2bf77118c18fd3e2de4c46e5037998dda2db2e6e
Anor Neto <anornetoo@gmail.com>
2021-07-27 01:32:41 -0300

    * Document Routes

--- api/routers/equipment_route.py
+++ api/routers/equipment_route.py
@@ -11,25 +11,25 @@ route_settings = RouteSettings()
 
 router = APIRouter(prefix = route_settings.vessels)
 
-@router.get("/{vessel_code}" + route_settings.equipments, response_model=List[EquipmentRead], status_code= status.HTTP_200_OK)
+@router.get("/{vessel_code}" + route_settings.equipments,tags=["Vessels"], response_model=List[EquipmentRead], status_code= status.HTTP_200_OK)
 async def get_vessel_equipments(vessel_code:str, filter_inactive: bool = True, db = Depends( get_db)):
   bus = EquipmentBus(db)
   db_active_equipments_list = bus.get_equipments(vessel_code= vessel_code, filter_inactive= filter_inactive)
   return db_active_equipments_list
 
-@router.post("/{vessel_code}" + route_settings.equipments, response_model=EquipmentRead, status_code= status.HTTP_201_CREATED)
+@router.post("/{vessel_code}" + route_settings.equipments,tags=["Vessels"], response_model=EquipmentRead, status_code= status.HTTP_201_CREATED)
 async def create_vessel_equipment(vessel_code:str, equipment_create: EquipmentCreate, db = Depends( get_db)):
   bus = EquipmentBus(db)
   db_equipment = bus.create_equipment(vessel_code= vessel_code, equipment_create= equipment_create)
   return db_equipment
 
-@router.put("/{vessel_code}" + route_settings.equipments, response_model=EquipmentRead, status_code= status.HTTP_200_OK)
+@router.put("/{vessel_code}" + route_settings.equipments,tags=["Vessels"], response_model=EquipmentRead, status_code= status.HTTP_200_OK)
 async def update_vessel_equipment(vessel_code:str, equipment_update: EquipmentUpdate, db = Depends( get_db)):
   bus = EquipmentBus(db)
   db_equipment = bus.update_equipment(vessel_code= vessel_code, equipment_update= equipment_update)
   return db_equipment
 
-@router.delete("/{vessel_code}" + route_settings.equipments + "/deactivate", response_class=Response, status_code= status.HTTP_204_NO_CONTENT)
+@router.delete("/{vessel_code}" + route_settings.equipments + "/deactivate",tags=["Vessels"], response_class=Response, status_code= status.HTTP_204_NO_CONTENT)
 async def delete_vessel_equipments(vessel_code:str, eqps_delete_list: List[EquipmentDelete], db = Depends( get_db)):
   bus = EquipmentBus(db)
   bus.delete_equipments(vessel_code= vessel_code, eqps_delete_list= eqps_delete_list)
--- api/routers/vessel_route.py
+++ api/routers/vessel_route.py
@@ -10,19 +10,51 @@ from api.schemas.vessel_schema import VesselRead,VesselCreate
 route_settings = RouteSettings()
 router = APIRouter(prefix = route_settings.vessels)
 
-@router.get("", response_model=List[VesselRead], status_code= status.HTTP_200_OK)
+@router.get(
+  "",
+  response_model=List[VesselRead],
+  status_code= status.HTTP_200_OK,
+  tags=["Vessels"],
+  summary="Get all vessels",
+  description="Return all vessels and its active equipments",
+  responses={
+    200: {"model": List[VesselRead], "description": "List of vessels and its active equipments"}
+  }
+)
 def get_all_vessels( db = Depends( get_db )):
   bus = VesselBus(db)
   return bus.get_vessels()
 
-@router.get("/{vessel_code}", response_model=VesselRead, status_code= status.HTTP_200_OK)
+@router.get(
+  "/{vessel_code}",
+  tags=["Vessels"],
+  response_model=VesselRead,
+  status_code= status.HTTP_200_OK,
+  summary="Get vessel by code",
+  description="Return vessel with given code and its active equipments",
+  responses={
+    200 : {"model": VesselRead, "description": "The vessel requested by code"},
+    404:  {"description": "Vessel not Found", "content":{ "application/json": { "example": {"detail": "Vessel not found"} } }}
+  }
+)
 def get_vessel_by_code(vessel_code:str, db = Depends( get_db )):
   bus = VesselBus(db)
   db_vessel = bus.get_vessel_by_code(vessel_code)
   return db_vessel
 
 
-@router.post("", response_model=VesselRead, status_code= status.HTTP_201_CREATED)
+@router.post(
+  "",
+  tags=["Vessels"],
+  response_model=VesselRead,
+  status_code= status.HTTP_201_CREATED,
+  summary="Create a vessel",
+  description="Create a vessel with given code and returns it",
+  responses={
+    201: {"model": VesselRead, "description": "The vessel created"},
+    409: {"description": "Vessel already registered", "content":{ "application/json": { "example": {"detail": "Vessel already registered"} } }}
+  }
+)
 def create_vessel(vessel_create: VesselCreate, db = Depends( get_db )):
   bus = VesselBus(db)
   db_vessel = bus.create_vessel(vessel_create)
